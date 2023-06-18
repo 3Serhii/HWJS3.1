f1a32e1b54d2d3451d1a9a41a7ca40b3d32e9ea0
SERHII <4sergeyvoropaev@gmail.com>
2023-06-18 17:45:36 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//get numbers + validation (isNaN)
+//getOperand + validation by array
+//calculation
+//show result
+
+let operator1 = getNumber();
+let operator2 = getNumber();
+let operand = getOperand();
+let res = calc();
+
+alert(`${operator1} + ${operator2} = ${res}`);
+
+function getNumber() {
+    let numb = Number(prompt("Type number"));
+    if (isNaN(numb)) {
+        alert("Wrong action. Please type a number");
+        return getNumber();
+    }
+    return numb
+}
+
+
+function getOperand() {
+    let operand = prompt("Type operand");
+    let operands = ["+", "-", "*", "/"];
+    if (!operands.includes(operand)) {
+        alert('Wrong action. Please type +,-,*,/');
+        return getOperand();
+    }
+    return operand
+}
+
+function calc() {
+    let result
+    switch (operand) {
+        case "+":
+            result = operator1 + operator2
+            break;
+        case "-":
+            result = operator1 - operator2
+            break;
+        case "*":
+            result = operator1 * operator2
+            break;
+        case "/":
+            result = operator1 / operator2
+            break;
+    }
+    return result
+}
+
